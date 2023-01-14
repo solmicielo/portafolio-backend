335ef3ed70976dc6ac4474de62d2be3a7d4bd868
solmicielo <carreterosoledad@gmail.com>
2023-01-14 05:38:06 -0300

    web config

--- src/main/java/com/solcarretero/portafolio/WebConfig.java
+++ src/main/java/com/solcarretero/portafolio/WebConfig.java
@@ -1,10 +1,14 @@
 
 package com.solcarretero.portafolio;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@EnableWebMvc
 
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
     
     @Override
